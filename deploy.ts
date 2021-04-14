f716dc7ed93f04d6202cbdfbd6cada86d5be9903
DjDeveloperr <djsandhu2005@gmail.com>
2021-04-14 12:16:34 +0530

    try fix

--- deploy.ts
+++ deploy.ts
@@ -2,17 +2,27 @@ import { serve } from "https://deno.land/x/sift@0.2.0/mod.ts";
 
 serve({
   "/": async (req) => {
-    return new Response(await fetch("./index.html").then((r) => r.text()), {
-      headers: {
-        "content-type": "text/html",
-      },
-    });
+    return new Response(
+      await fetch(new URL("./index.html", import.meta.url)).then((r) =>
+        r.text()
+      ),
+      {
+        headers: {
+          "content-type": "text/html",
+        },
+      }
+    );
   },
   "/bundle.js": async (req) => {
-    return new Response(await fetch("./bundle.js").then((r) => r.text()), {
-      headers: {
-        "content-type": "application/javascript",
-      },
-    });
+    return new Response(
+      await fetch(new URL("./bundle.js", import.meta.url)).then((r) =>
+        r.text()
+      ),
+      {
+        headers: {
+          "content-type": "application/javascript",
+        },
+      }
+    );
   },
 });
