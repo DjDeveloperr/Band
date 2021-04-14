596b5c76a7055e6235d65aba16f6bceb9e2dee10
DjDeveloperr <djsandhu2005@gmail.com>
2021-04-14 12:11:13 +0530

    fix

--- deploy.ts
+++ deploy.ts
@@ -3,10 +3,10 @@ import { serve, serveStatic } from "https://deno.land/x/sift@0.2.0/mod.ts";
 serve({
   "/": serveStatic("./index.html", {
     baseUrl: import.meta.url,
-    intervene: (res) => {
-      res.headers.set("content-type", "text/html");
-      return res;
-    },
+    // intervene: (res) => {
+    //   res.headers.set("content-type", "text/html");
+    //   return res;
+    // },
   }),
   "/bundle.js": serveStatic("./bundle.js", {
     baseUrl: import.meta.url,
